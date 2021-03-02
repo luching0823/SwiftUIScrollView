7f37f7169f64d738a4627225997b698adec0d31a
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-02 15:32:09 +0800

    Initial Commit

---
+++ SwiftUIScrollView/SwiftUIScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewApp.swift
+//  SwiftUIScrollView
+//
+//  Created by 廖昱晴 on 2021/3/2.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
