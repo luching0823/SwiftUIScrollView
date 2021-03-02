1e40c4e4debbf2b0d53423bec250ef3c37e49f77
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-02 16:04:38 +0800

    init SwiftUIScrollView

--- SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/ContentView.swift
@@ -9,8 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            
+        VStack {
+            VStack {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    VStack(alignment: .leading) {
+                        HStack {
+                                VStack(alignment: .leading) {
+                                    Text("Monday, AUG 20".uppercased())
+                                        .foregroundColor(.secondary)
+                                    Text("Your Reading")
+                                        .font(.largeTitle)
+                                        .fontWeight(.black)
+                                        .foregroundColor(.primary)
+                                }
+                                Spacer()
+                            }
+                        .padding()
+                    
+                        
+                        HStack {
+                            Group {
+                                CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                                CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel")
+                                CardView(image: "flutter-app", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                                CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+                            }
+                            .frame(width: 300)
+                        }
+                    }
+                }
+            }
+            Spacer()
+        }
     }
 }
 
